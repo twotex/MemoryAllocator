564127602378540982aa8f028a36440edf1bc3fa
twotex <41358927+twotex@users.noreply.github.com>
2020-03-10 17:14:33 -0700

    Revamped code based on my new understanding the requirements. mymalloc's implementation still needs to be completed.

--- MemoryAllocator.c
+++ MemoryAllocator.c
@@ -34,48 +34,69 @@ int main()
 void my_initialize_heap(int size)
 {
 	void* allocated_storage = malloc(size);
-	free_head = allocated_storage;
+	free_head = (struct Block*)allocated_storage;
 	free_head->block_size = size - OVERHEAD_SIZE;
 	free_head->next_block = NULL;
 }
 
 void* my_malloc(int size)
 {
-	struct Block *tempHead;
-	tempHead = free_head;
-
-	while (tempHead != NULL)
+	if (free_head->block_size % VOID_PTR_SIZE == 0 && free_head->block_size >= size && free_head->block_size > 0)
 	{
-		if (tempHead->block_size % VOID_PTR_SIZE == 0 && tempHead->block_size >= size)
+		if (free_head->block_size > (size + OVERHEAD_SIZE + VOID_PTR_SIZE))
 		{
-			if (tempHead->block_size > size)
-			{
-				int extraSpace = tempHead->block_size - size;
-				struct Block *proceedingBlock = tempHead->next_block;
-				proceedingBlock->block_size = extraSpace;
-				proceedingBlock->next_block = NULL;
-			}
 
-			else
-			{
-				free_head = free_head->next_block;
-			}
-		}
-
-		else
-		{
-			tempHead = tempHead->next_block;
 		}
 	}
+
+	else
+	{
+		return nullptr;
+	}
+
 	
 }
 
 void my_free(void *data)
 {
-	struct Block *tempBlock = data;
-	struct Block *blockToFree = tempBlock - OVERHEAD_SIZE;
+	struct Block *tempBlock = (struct Block*) data;
+	struct Block *blockToFree = tempBlock--;
 
-	(*blockToFree).next_block = free_head;
-	(*blockToFree).block_size = 0;
+	blockToFree->block_size = blockToFree->block_size + free_head->block_size + OVERHEAD_SIZE;
+	blockToFree->next_block = null;
 	free_head = blockToFree;
 }
+
+
+
+
+/*
+
+struct Block *tempHead;
+tempHead = free_head;
+while (tempHead != NULL)
+{
+	if (tempHead->block_size % VOID_PTR_SIZE == 0 && tempHead->block_size >= size)
+	{
+		if (tempHead->block_size > size)
+		{
+			int extraSpace = tempHead->block_size - size;
+			struct Block *proceedingBlock = tempHead->next_block;
+			proceedingBlock->block_size = extraSpace;
+			proceedingBlock->next_block = NULL;
+		}
+
+		else
+		{
+			free_head = free_head->next_block;
+			tempHead = free_head;
+		}
+	}
+
+	else
+	{
+		tempHead = tempHead->next_block;
+	}
+}
+
+*/
